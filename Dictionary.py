f50edc6553de472d8c7aa677be06e50b1042641d
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-08-21 14:28:24 +0530

    Create Dictionary.py
    This Code Writes you find information as it is Dictionary and this code is very useful for find value of a particular variable.

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+# python
+
+mydict = {
+    "firstName": "Python",
+    "lastName": "Hub",
+    "rankOnInsta": 1
+}
+print(mydict)
+
+
+print(mydict["firstName"])
+print(mydict["lastName"])
+print(mydict["rankOnInsta"])
+
+for x in mydict:
+    print(x)
+
+    for  x, y in mydict.items():
+        print(x, y)
+
+
