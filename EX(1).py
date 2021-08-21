b3f7b60adbe085b9d6cea962ff5ab2abf652a6b2
SARR-IN <82797844+SARR-IN@users.noreply.github.com>
2021-08-21 14:30:14 +0530

    Create EX(1).py
    This is an example of practice in the Python language.

---
+++ EX(1).py
@@ -0,0 +1,21 @@
+# python
+
+var = "James Bond"
+print(var[2::-1])
+
+L = [1, 2, 3]
+L.extend((4, 5, 6))
+print(L)
+
+i = 0
+while i< 5:
+    print(i, end = " ")
+    i += 1
+    if i == 3:
+    
+else:
+    print(0)
+
+a = 100
+b = 5
+print(a//b*a/b)
